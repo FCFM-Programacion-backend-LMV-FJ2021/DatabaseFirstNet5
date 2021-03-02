867e16c4a576d21d610dab5ddddde50fd0f3148d
Rolando <rolando.fcfm2020@gmail.com>
2021-03-01 19:34:47 -0600

    Before Refactor

--- DatabaseFirstDWB_LMV/Program.cs
+++ DatabaseFirstDWB_LMV/Program.cs
@@ -2,12 +2,13 @@
 using System;
 using System.Linq;
 using DatabaseFirstDWB_LMV.NorthwindData;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatabaseFirstDWB_LMV
 {
     class Program
     {
-
+        public static NORTHWNDContext dbContext = new NORTHWNDContext();
         public static void InitialTest()
         {
 
@@ -55,6 +56,12 @@ namespace DatabaseFirstDWB_LMV
             //Aqui materializamos el query
             var output = employeeQry.ToList();
 
+            //var od = new NORTHWNDContext().Products.Include(i => i.OrderDetails)
+            //    .Where(W => W.ProductId == 1)
+            //    .SelectMany(s => s.OrderDetails)
+            //    .Select(s => s.Order);
+            //var o = od.ToList();
+
             output.ForEach(fe => Console.WriteLine("Name: " + fe.FirstName + " Title: " + fe.Title));
 
         }
@@ -74,27 +81,69 @@ namespace DatabaseFirstDWB_LMV
             var output = employeeQry.ToList();
 
         }
+
+        public static void Excercise4()
+        {
+            // UPDATE Employees SET NAME = ‘Nadia’ WHERE ID = 1; 
+            
+            var currentEmployee = dbContext.Employees.Where(x => x.EmployeeId == 1).First();
+            currentEmployee.FirstName = "Mauricio";
+            dbContext.Employees.Update(currentEmployee);
+            dbContext.SaveChanges();
+            Console.WriteLine("ID: " + currentEmployee.EmployeeId + " Nombre: " + currentEmployee.FirstName);
+
+            var currentEmployee2 = dbContext.Employees.Where(x => x.EmployeeId == 2).First();
+            currentEmployee2.FirstName = "Rolando";
+            dbContext.SaveChanges();
+
+        }
+
+        public static void Excercise5()
+        {
+            //insert into Products (ProductName, UnitPrice) values ('coca cola', 12.50)
+            var newProduct = new NorthwindData.Product();
+            newProduct.ProductName = "Coca cola";
+            newProduct.UnitPrice = 12.50m;
+
+            //dbContext.Add(newProduct);
+            dbContext.Products.Add(newProduct);
+            dbContext.SaveChanges();
+            
+            
+
+        }
+
+        public static void Excercise6(int id)
+        {
+            var currentEmployee = dbContext.Employees.Where(x => x.EmployeeId == id).First();
+
+            dbContext.Employees.Remove(currentEmployee);
+            dbContext.SaveChanges();
+
+        }
+
+        //  Obtener los productos, el cliente y el empleado por Id de Order
+        public static void Excercise7(int orderID)
+        {
+            var qry = dbContext.Orders.Where(w => w.OrderId == orderID).Select(s => new
+            {
+
+                Empleado = s.Employee.FirstName,
+                Cliente = s.Customer.ContactName,
+                Products = s.OrderDetails.Select(sel => sel.Product.ProductName)
+            });
+
+            var result = qry.ToList();
+        }
         static void Main(string[] args)
         {
-            Excercise1();
-            Excercise2();
-            Excercise3();
-            //ejemplo insert
-            Employee employee = new Employee();
-            employee.FirstName = "Rolando";
-            employee.City = "Monterrey";
-            //...
-
-            var dbContext = new NORTHWNDContext();
-            dbContext.Employees.Add(employee);
-            dbContext.SaveChanges();
-
-
-            //ejemplo delete
-
-            var product = dbContext.Products.Where(w => w.ProductId == 1).First();
-            dbContext.Products.Remove(product);
-            dbContext.SaveChanges();
+            //Excercise1();
+            //Excercise2();
+            //Excercise3();
+            //Excercise4();
+            //Excercise5();
+            //Excercise6(9);
+            Excercise7(10248);
 
         }
     }
